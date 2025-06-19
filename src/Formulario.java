95363ee8f0e4f27886f99a153f50d0a2679b5bae
POO <josselyn.ayoharo@epn.edu.ec>
2025-06-19 09:53:56 -0500

    Josselyn Ayo :)

---
+++ src/Formulario.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Formulario extends JFrame {
+    private JPanel Formulario;
+    private JTextField NombreField;
+    private JTextField ApellidoField2;
+    private JTextField DireccionField3;
+    private JTextField TelefonoField4;
+    private JTextField AñoNacimientoField5;
+    private JTextField EstaturaField6;
+    private JButton accion1Button;
+    private JButton accion2Button;
+    private JLabel resultadoLabel;
+
+    public Formulario() {
+        setTitle("FORMULARIO BASICO");
+        setSize(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(Formulario);
+        setVisible(true);
+
+
+
+        accion1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String datos = "Nombre: " + NombreField.getText()
+               + "\nApellido:" + ApellidoField2.getText()
+                +"\nDireccion:" +DireccionField3.getText()
+                + "\nTelefono:" +TelefonoField4.getText()
+                 +"\nAño de Nacimiento: "+ AñoNacimientoField5.getText()
+                + "\nEstatura: "+ EstaturaField6.getText();
+                JOptionPane.showMessageDialog(null,datos);
+            }
+        });
+
+        accion2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String datos = "Nombre: " + NombreField.getText()
+                        + "\nApellido:" + ApellidoField2.getText()
+                        +"\nDireccion:" +DireccionField3.getText()
+                        + "\nTelefono:" +TelefonoField4.getText()
+                        +"\nAño de Nacimiento: "+ AñoNacimientoField5.getText()
+                        + "\nEstatura: "+ EstaturaField6.getText();
+                resultadoLabel.setText(datos);
+
+            }
+        });
+    }
+}
